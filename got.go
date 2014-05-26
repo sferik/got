83d156b42ce036ae52953aaec0ca68a4f8136714
Erik Michaels-Ober <sferik@gmail.com>
2014-05-26 16:52:42 +0200

    go fmt -w

--- got.go
+++ got.go
@@ -1,23 +1,27 @@
 package main
 
 import (
-  "flag"
-  "fmt"
+	"flag"
+	"fmt"
 )
 
 func ruler() {
-  var ruler string
-  for len(ruler) < 140 {
-    ruler += "----|"
-  }
-  fmt.Println(ruler)
+	var spacesToIndent = flag.Int("indent", 0, "the number of spaces to print before the ruler")
+	var ruler string
+	for i := 0; i < *spacesToIndent; i++ {
+		ruler += " "
+	}
+	for len(ruler) < 140 {
+		ruler += "----|"
+	}
+	fmt.Println(ruler)
 }
 
 func main() {
-  flag.Parse()
-  args := flag.Args()
-  switch args[0] {
-  case "ruler":
-    ruler()
-  }
+	flag.Parse()
+	args := flag.Args()
+	switch args[0] {
+	case "ruler":
+		ruler()
+	}
 }
